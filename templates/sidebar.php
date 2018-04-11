e8089a194a55b1f6b33207da4e2b11f09a1c5566
NsrWissam <wissam.nasser@student.ehb.be>
2018-04-12 01:02:57 +0200

    SIDEBAR DROPDOWN EINDELIJK GELUKT
    CELEBRATION TIMES :D

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,65 +1,37 @@
 <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar " style="background-color: #5a6268;">
+    <div id="MainMenu" style="color: white;">
 
-    <ul class="nav nav-pills flex-column">
-        <li class="nav-item">
-            <p class="navbar-text mb-0 mt-3">Categories</p>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">Travel</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">Technology</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">Photography</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">Architecture</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">Rave Culture</a>
-        </li>
-    </ul>
 
-    <ul class="nav nav-pills flex-column">
-        <li class="nav-item">
-            <p class="navbar-text mb-0 mt-3">Month Archive</p>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">January</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">February</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">March</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">April</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">May</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">June</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">July</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">August</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">September</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">October</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">November</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link-sidebar" href="#">December</a>
-        </li>
-    </ul>
+            <a href="#collapse1" class="navbar-text mb-0 mt-3 clg" data-parent="#MainMenu" data-toggle="collapse">Categories
+                <i class="fa fa-caret-down"></i></a>
+        <div class="row">
+            <div class="collapse" id="collapse1">
+
+                <a class="nav-link-sidebar" href="#">Travel</a>
+                <a class="nav-link-sidebar" href="#">Technology</a>
+                <a class="nav-link-sidebar" href="#">Photography</a>
+                <a class="nav-link-sidebar" href="#">Architecture</a>
+                <a class="nav-link-sidebar" href="#">Rave Culture</a>
+            </div>
+        </div>
+
+            <a href="#collapse2" class="navbar-text mb-0 mt-3 clg" data-parent="#MainMenu" data-toggle="collapse">Month
+                Archive <i class="fa fa-caret-down"></i></a>
+        <div class="row">
+            <div class="collapse" id="collapse2">
+
+                <a class="nav-link-sidebar" href="#">January</a>
+                <a class="nav-link-sidebar" href="#">February</a>
+                <a class="nav-link-sidebar" href="#">March</a>
+                <a class="nav-link-sidebar" href="#">April</a>
+                <a class="nav-link-sidebar" href="#">May</a>
+                <a class="nav-link-sidebar" href="#">June</a>
+                <a class="nav-link-sidebar" href="#">July</a>
+                <a class="nav-link-sidebar" href="#">August</a>
+                <a class="nav-link-sidebar" href="#">September</a>
+                <a class="nav-link-sidebar" href="#">October</a>
+                <a class="nav-link-sidebar" href="#">November</a>
+                <a class="nav-link-sidebar" href="#">December</a>
+            </div>
+        </div>
 </nav>
